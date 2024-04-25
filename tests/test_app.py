57aa2056ac4cdf1f20c3b48f9ed3f02d8c103ba9
Etore-BeS <etorebraga@usp.br>
2024-04-25 10:47:02 -0300

    Updating endpoints to use the real database

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_root(client):
     response = client.get('/')  # Act
@@ -31,24 +33,30 @@ def test_create_user(client):
         'email': 'test@test.com',
         'id': 1,
     }  # Assert
+    
+
+def test_create_user_already_exists(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'Teste',
+            'email': 'test@test.com',
+            'password': '123456',
+        },
+    )  # Act
+    
+    assert response.status_code == HTTPStatus.BAD_REQUEST  # Assert
+    assert response.json() == {'detail': 'Username already exists'}  # Assert
 
 
 def test_read_users(client):
     response = client.get('/users/')  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'user',
-                'email': 'test@test.com',
-                'id': 1,
-            }
-        ]
-    }  # Assert
+    assert response.json() == {'users': []}  # Assert
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -79,32 +87,38 @@ def test_update_user_not_found(client):
     assert response.status_code == HTTPStatus.NOT_FOUND  # Assert
 
 
-def test_read_user(client):
+def test_read_user(client, user):
     response = client.get('/users/1')  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {
-        'username': 'user',
+        'username': 'Teste',
         'email': 'test@test.com',
         'id': 1,
     }  # Assert
 
 
-def test_read_user_not_found(client):
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')  # Act
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_read_user_not_found(client, user):
     response = client.get('/users/0')  # Act
 
     assert response.status_code == HTTPStatus.NOT_FOUND  # Assert
     assert response.json() == {'detail': 'User not found'}  # Assert
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {'message': 'User deleted'}  # Assert
 
 
-def test_delete_user_not_found(client):
+def test_delete_user_not_found(client, user):
     response = client.delete('/users/0')  # Act
 
     assert response.status_code == HTTPStatus.NOT_FOUND  # Assert
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,7 +1,24 @@
 from sqlalchemy import select
+from sqlalchemy.orm import Session
 
+from fast_zero.database import get_session
 from fast_zero.models import User
+from fast_zero.settings import Settings
 
+
+def test_get_session():
+    session_gen = get_session()
+    session = next(session_gen)
+    
+    assert isinstance(session, Session)
+    assert str(session.get_bind().url) == Settings().DATABASE_URL
+
+    try:
+        next(session_gen)
+    except StopIteration:
+        pass
+    else:
+        assert False, "get_session generator yielded more than one value"
 
 def test_create_user(session):
     new_user = User(username='user', password='123456', email='test@test.com')
@@ -11,3 +28,5 @@ def test_create_user(session):
     user = session.scalar(select(User).where(User.username == 'user'))
 
     assert user.username == 'user'
+    
+    
